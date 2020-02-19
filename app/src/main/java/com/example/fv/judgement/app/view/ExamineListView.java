b6f1f31a1aea1b5cf0c7703b5a01644da3a03ad5
zhangcl <lihe@midtao.com>
2020-02-19 21:12:05 +0800

    待我审批

--- app/src/main/java/com/example/fv/judgement/app/view/ExamineListView.java
+++ app/src/main/java/com/example/fv/judgement/app/view/ExamineListView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
@@ -14,18 +15,29 @@ import com.example.fv.judgement.app.model.ExamineModel;
 
 import zuo.biao.library.base.BaseView;
 import zuo.biao.library.util.CommonUtil;
+import zuo.biao.library.util.StringUtil;
 
-public class ExamineListView extends BaseView<ExamineModel>
+public class ExamineListView extends BaseView<ExamineModel> implements View.OnClickListener
 {
     //构造需继承样式继承列表样式R.layout.user_view
     public ExamineListView(Activity context, ViewGroup parent) {
-        super(context, R.layout.activity_leave_edit, parent);
+        super(context, R.layout.examine_view, parent);
     }
     public ImageView ivUserViewHead; //创建画面所需控件
+    public ImageView ivUserViewStar;
+    public TextView tvUserViewSex;
+    public TextView tvUserViewName;
+    public TextView tvUserViewId;
+    public TextView tvUserViewNumber;
 
-     //   创建画面控件关联
+     //创建画面控件关联
     public View createView() {
-       // ivUserViewHead = findView(R.id.ivUserViewHead, this);
+        ivUserViewHead = findView(R.id.ivUserViewHead, this);
+        tvUserViewSex = findView(R.id.tvUserViewSex, this);
+        tvUserViewName = findView(R.id.tvUserViewName);
+        tvUserViewId = findView(R.id.tvUserViewId);
+        tvUserViewNumber = findView(R.id.tvUserViewNumber);
+
         return super.createView();
     }
     public void bindView(ExamineModel data_){
@@ -37,7 +49,14 @@ public class ExamineListView extends BaseView<ExamineModel>
                 ivUserViewHead.setImageBitmap(CommonUtil.toRoundCorner(bitmap, bitmap.getWidth()/2));
             }
         });
+        tvUserViewSex.setBackgroundResource(data.getSex() == ExamineModel.SEX_FEMALE
+                ? R.drawable.circle_pink : R.drawable.circle_blue);
+        tvUserViewSex.setText(data.getSex() == ExamineModel.SEX_FEMALE ?  "女" : "男");
+        tvUserViewSex.setTextColor(getColor(data.getSex() == ExamineModel.SEX_FEMALE ? R.color.pink : R.color.blue));
 
+        tvUserViewName.setText(StringUtil.getTrimedString(data.getName()));
+        tvUserViewId.setText("ID:" + data.getId());
+        tvUserViewNumber.setText("Phone:" + StringUtil.getNoBlankString(data.getPhone()));
     }
     public void onClick(View v) {
     }
