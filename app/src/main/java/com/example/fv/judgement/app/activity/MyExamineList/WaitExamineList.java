df5f55ca2813b48cc6816db8d229cb51c2aea2a4
zhangcl <lihe@midtao.com>
2020-02-21 21:09:30 +0800

    待我审批

--- app/src/main/java/com/example/fv/judgement/app/activity/MyExamineList/WaitExamineList.java
+++ app/src/main/java/com/example/fv/judgement/app/activity/MyExamineList/WaitExamineList.java
@@ -16,6 +16,7 @@ import zuo.biao.library.base.BaseActivity;
 import com.alibaba.fastjson.JSONObject;
 import com.example.fv.judgement.R;
 import com.example.fv.judgement.app.adapter.ExamineListAdapter;
+import com.example.fv.judgement.app.application.GlobalInformationApplication;
 import com.example.fv.judgement.app.model.ExamineModel;
 import com.example.fv.judgement.app.view.ExamineListView;
 
@@ -48,6 +49,8 @@ import org.ksoap2.serialization.SoapObject;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class WaitExamineList extends BaseHttpListFragment<ExamineModel, ListView, ExamineListAdapter> implements CacheCallBack<ExamineModel>
 {
@@ -116,32 +119,12 @@ public class WaitExamineList extends BaseHttpListFragment<ExamineModel, ListView
         @Override
         public void initData() {//必须调用
             super.initData();
-        //    List<ExamineModel> list=  GetExamineData();
-        //    setList(list);
-        }
-        public  List<ExamineModel>  GetExamineData()
-        {
-            String methodName = "GetPendingInfo";
-            SoapObject soapObject = new SoapObject(GlobalVariableApplication.SERVICE_NAMESPACE,methodName);
-            soapObject.addProperty("pasgeIndex","1");
-            soapObject.addProperty("pageSize","5");
-            soapObject.addProperty("code","40");
-            soapObject.addProperty("userID","91");
-            soapObject.addProperty("menuID","4");
-            soapObject.addProperty("iosid","00000000-0000-0000-0000-000000000000");
-            HttpRequest httpres= new HttpRequest();
-            String jsonData = httpres.httpWebService_GetString(methodName,soapObject);
-            List<ExamineModel> listExaData=new ArrayList<ExamineModel>();
-
-            Type type = new TypeToken<List<ExamineModel>>(){}.getType();
-            listExaData = new Gson().fromJson(jsonData,type);
-
-            return listExaData;
         }
 
         @Override
         public void getListAsync(final int page) {
             //实际使用时用这个，需要配置服务器地址  HttpRequest.getUserList(range, page, -page, this);
+            LoginUserModel userModel = GlobalInformationApplication.getInstance().getCurrentUser();
 
             int pageindex = page;
             pageindex++;
@@ -162,8 +145,8 @@ public class WaitExamineList extends BaseHttpListFragment<ExamineModel, ListView
             SoapObject soapObject = new SoapObject(GlobalVariableApplication.SERVICE_NAMESPACE,methodName);
             soapObject.addProperty("pasgeIndex",pageindex);
             soapObject.addProperty("pageSize",CurentPageCount);
-            soapObject.addProperty("code","45");
-            soapObject.addProperty("userID","96");
+            soapObject.addProperty("code",userModel.getCode());
+            soapObject.addProperty("userID",userModel.getUserNO());
             soapObject.addProperty("menuID","4");
             soapObject.addProperty("iosid","00000000-0000-0000-0000-000000000000");
             HttpRequest httpres= new HttpRequest();
@@ -174,7 +157,7 @@ public class WaitExamineList extends BaseHttpListFragment<ExamineModel, ListView
             listExaData = new Gson().fromJson(jsonData,type);
 
             //框架是 每次取增量数据
-            onHttpResponse(-page, page > 3 ? null :JSON.toJSONString(listExaData), null);
+            onHttpResponse(-page, JSON.toJSONString(listExaData), null);
             //仅测试用<<<<<<<<<<<
 //            new Handler().postDelayed(new Runnable() {
 //
