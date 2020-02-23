e79cf72424f2e87ad715c1a2daebee81b74aa140
youkare <lihe@midtao.com>
2020-02-23 17:49:06 +0800

    首页加按钮

--- app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
+++ app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
@@ -2,14 +2,36 @@ package com.example.fv.judgement.app.activity.Home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fv.judgement.R;
 
 public class PageHome extends AppCompatActivity {
-
+    private Button myApplybtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_home);
+
+        myApplybtn  = findViewById(R.id.myApply);
+        myApplybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //此处写入操作
+                            }
+                        });
+
+                    }
+                }).start();
+            }
+        });
+
     }
 }
