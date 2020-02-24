bf01d6d81dee2b665d98b0916883328848eb7272
youkare <lihe@midtao.com>
2020-02-24 10:05:11 +0800

    首页加按钮

--- app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
+++ app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
@@ -1,21 +1,35 @@
 package com.example.fv.judgement.app.activity.Home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.fv.judgement.R;
+import com.example.fv.judgement.app.activity.MyExamineList.MainTabActivity;
 
 public class PageHome extends AppCompatActivity {
     private Button myApplybtn;
+    private Button waitmyApplybtn;
+    private Button waitmyReadbtn;
+    private Button leavebtn;
+    private Button gooutbtn;
+    private Button businessTripbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_home);
 
-        myApplybtn  = findViewById(R.id.myApply);
-        myApplybtn.setOnClickListener(new View.OnClickListener() {
+        myApplybtn = findViewById(R.id.myApply);
+        waitmyApplybtn = findViewById(R.id.waitmyApply);
+        waitmyReadbtn = findViewById(R.id.waitmyRead);
+        leavebtn = findViewById(R.id.leave);
+        gooutbtn = findViewById(R.id.goout);
+        businessTripbtn = findViewById(R.id.businessTrip);
+
+        waitmyApplybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
@@ -24,7 +38,9 @@ public class PageHome extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                //此处写入操作
+                                Intent intent = new Intent();
+                                intent.setClass(PageHome.this, MainTabActivity.class);
+                                startActivity(intent);
                             }
                         });
 
