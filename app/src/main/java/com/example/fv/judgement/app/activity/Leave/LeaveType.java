7a618260328a63912247b50a54a4ebd4f3ccbcd6
MidtaoLH <46152301+MidtaoLH@users.noreply.github.com>
2020-02-24 09:04:04 +0800

    请假编辑

---
+++ app/src/main/java/com/example/fv/judgement/app/activity/Leave/LeaveType.java
@@ -0,0 +1,18 @@
+package com.example.fv.judgement.app.activity.Leave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.fv.judgement.R;
+
+import zuo.biao.library.base.BaseHttpListActivity;
+import zuo.biao.library.interfaces.OnBottomDragListener;
+
+public class LeaveType  extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_type);
+    }
+}
