a77542fa47d1b63e6d5165f2194cad4b6c64b4f6
youkare <lihe@midtao.com>
2020-02-26 12:07:58 +0800

    首页增加公告跳转

--- app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
+++ app/src/main/java/com/example/fv/judgement/app/activity/Home/PageHome.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.fv.judgement.R;
+import com.example.fv.judgement.app.activity.BusinessTrip.BusinessTripTabbar;
 import com.example.fv.judgement.app.activity.Leave.LeaveTabActivity;
 import com.example.fv.judgement.app.activity.MyExamineList.MainTabActivity;
+import com.example.fv.judgement.app.activity.Notice.MainNoticeActivity;
 
 public class PageHome extends AppCompatActivity {
     private Button myApplybtn;
@@ -17,6 +19,7 @@ public class PageHome extends AppCompatActivity {
     private Button leavebtn;
     private Button gooutbtn;
     private Button businessTripbtn;
+    private Button noticebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class PageHome extends AppCompatActivity {
         leavebtn = findViewById(R.id.leave);
         gooutbtn = findViewById(R.id.goout);
         businessTripbtn = findViewById(R.id.businessTrip);
+        noticebtn = findViewById(R.id.notice);
 
         waitmyApplybtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +47,22 @@ public class PageHome extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(PageHome.this, LeaveTabActivity.class);
+                startActivity(intent);
+            }
+        });
+        businessTripbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(PageHome.this, BusinessTripTabbar.class);
+                startActivity(intent);
+            }
+        });
+        noticebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(PageHome.this, MainNoticeActivity.class);
                 startActivity(intent);
             }
         });
